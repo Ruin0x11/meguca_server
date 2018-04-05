628eeeef191d67813f30a5e14b7bdb46abd505cd
Ian Pickering <ipickering2@gmail.com>
2018-04-05 10:44:37 -0700

    add site

---
+++ site/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkfifo /tmp/inotifywait.fifo
+mkdir /srv/site/md /srv/site/rendered
+
+inotifywait -drq -o /tmp/inotifywait.fifo -e create -e modify /srv/site/md
+
+trap "true" PIPE
+cat /tmp/inotifywait.fifo | 
+    while read path op file; do 
+        echo $path$file $op
+    done
+trap PIPE
