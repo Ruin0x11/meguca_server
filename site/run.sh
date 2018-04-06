d8e4248f4b6cddd6569cc06a1916ee368ea5f419
Ian Pickering <ipickering2@gmail.com>
2018-04-06 09:02:13 -0700

    hide rendered site files inside container

--- site/run.sh
+++ site/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkfifo /tmp/inotifywait.fifo
-mkdir -p /data/site/md /data/site/build /data/site/rendered
-cp -r /data/Makefile /data/template /data/site
+mkdir -p /data/site /data/site/build /data/site/rendered
+cp -r /data/Makefile /data/site
+cp -r /data/template /data/site/md
 cd /data/site
 
 echo "Building site..."
